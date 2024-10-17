8872cb1e1c75f5e8fcaddf783e18b13bc6d941cf
ManojDhaka57 <manojdhaka7599@gmail.com>
2024-10-17 17:07:07 +0530

    Update test case for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { ThemeProvider } from "./contexts/theme-context";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+  const linkElement = screen.getByText(/Home theme/i);
   expect(linkElement).toBeInTheDocument();
 });
